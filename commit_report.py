59b4f0b4f0f557a2874a5d2ae3cb2aa679681db6
Federico Fissore <federico@fissore.org>
2020-08-04 11:41:12 +0200

    Still trying to update translation report, to no avail

--- commit_report.py
+++ commit_report.py
@@ -2,15 +2,18 @@
 
 import os
 import sys
-import base64
+import json
 from github import Github
 
-TRANSLATION_REPORT_FILENAME = "translation_report.md"
+with open(os.getenv("GITHUB_EVENT_PATH")) as e:
+    github_event = json.load(e)
 
 github_token = sys.argv[1]
 g = Github(github_token)
-repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
-branch = sys.argv[2]
+repo = g.get_repo(github_event['pull_request']['head']['repo']['full_name'])
+branch = github_event['pull_request']['head']['ref']
+
+TRANSLATION_REPORT_FILENAME = "translation_report.md"
 
 previous_report_obj = repo.get_contents(TRANSLATION_REPORT_FILENAME, ref=branch)
 previous_report = previous_report_obj.decoded_content.decode("utf-8")
